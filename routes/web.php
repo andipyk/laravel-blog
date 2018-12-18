14e061f05dd3851cd549ad5bb6dd6f0afc5affeb
Andi Ganteng <andisyafrianda@gmail.com>
2018-12-18 16:58:38 +0700

    completed view, controller

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('products');
 });
+
+
+Route::resource('products', 'ProductController');
